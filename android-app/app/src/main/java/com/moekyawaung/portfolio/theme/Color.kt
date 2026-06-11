5b98b5c9b14b21df31fb0d4f24ae9b27c1af8991
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-06-11 15:06:18 +0630

    Create Color.kt

---
+++ android-app/app/src/main/java/com/moekyawaung/portfolio/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.moekyawaung.portfolio.theme
+
+import androidx.compose.ui.graphics.Color
+
+object NeonColors {
+    val neonCyan = Color(0xFF00F5FF)
+    val neonPink = Color(0xFFFF3DF2)
+    val neonYellow = Color(0xFFFFE600)
+    val neonGreen = Color(0xFF29FF88)
+    
+    val backgroundDark = Color(0xFF050816)
+    val backgroundDark2 = Color(0xFF0B1124)
+    val surfaceDark = Color(0xFF111731)
+    val surfaceDark2 = Color(0xFF182448)
+    
+    val textPrimary = Color(0xFFEAF2FF)
+    val textMuted = Color(0xFF9FB2D0)
+}
